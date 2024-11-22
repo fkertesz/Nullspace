0ebe388544e1cd3813a1a4723cf041f121dcdf4a
Zac Cowan <zcowan@bellarmine.edu>
2024-11-22 12:49:31 -0500

    11/22/2024 - Zac - Setup TailwindCSS

--- vectorspaceapp/src/App.js
+++ vectorspaceapp/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-6xl font-bold text-blue-700"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
