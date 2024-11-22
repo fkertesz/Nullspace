738f089c80f3054d0c322434d0536304ab7be92f
Zac Cowan <zcowan@bellarmine.edu>
2024-11-22 12:52:18 -0500

    11/22/2024 - Zac - Basic Task Component Rendered

--- vectorspaceapp/src/App.js
+++ vectorspaceapp/src/App.js
@@ -1,22 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Tasks from "./MinimumViableComponents/Tasks";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-6xl font-bold text-blue-700"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Tasks />
       </header>
     </div>
   );
